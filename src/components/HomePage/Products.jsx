038f1dedf0e6793364cbc217322ce4d0bf24f081
Rubayat Abdullah <114306964+rubayatabdullah@users.noreply.github.com>
2025-01-11 16:38:59 +0600

    review file path

--- src/components/HomePage/Products.jsx
+++ src/components/HomePage/Products.jsx
@@ -30,9 +30,7 @@ export default function Products() {
         <div className="product-rating-container">
           <img
             className="product-rating-stars"
-            src={`./public/images/ratings/rating-${
-              product.rating.stars * 10
-            }.png`}
+            src={`./images/ratings/rating-${product.rating.stars * 10}.png`}
           />
           <div className="product-rating-count link-primary">
             {product.rating.count}
